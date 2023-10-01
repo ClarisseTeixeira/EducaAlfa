47d9b8fafccca2b2fa9209178cd8caa7fb87e7a5
Thamires-Teixeira <121453460+Thamires-Teixeira@users.noreply.github.com>
2023-10-01 15:38:28 -0300

    arquivos

--- pomodoro/models.py
+++ pomodoro/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+class PomodoroSession(models.Model):
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    completed = models.BooleanField(default=False)
