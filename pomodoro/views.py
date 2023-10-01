3e37c9fe1c3ac54fca730b1d1fa0acd204ac65d0
Thamires-Teixeira <121453460+Thamires-Teixeira@users.noreply.github.com>
2023-10-01 16:48:56 -0300

    views

---
+++ pomodoro/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from datetime import datetime, timedelta
+from .models import PomodoroSession
+
+def pomodoro(request):
+    if request.method == 'POST':
+        
+        start_time = datetime.now()
+        end_time = start_time + timedelta(minutes=25)
+        PomodoroSession.objects.create(start_time=start_time, end_time=end_time)
+
+    sessions = PomodoroSession.objects.filter(completed=True).order_by('-start_time')[:4]
+    
+    return render(request, 'pomodoro/pomodoro.html', {'sessions': sessions})
