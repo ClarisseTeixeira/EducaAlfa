bb920a8492c03509e73c97eef54ab4f6fc3e6566
Clarisse <mclarissetc@gmail.com>
2023-10-01 17:06:54 -0300

    commit

--- revisao/admin.py
+++ revisao/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Flashcard, Revisao
 
 # Register your models here.
+
+@admin.register(Revisao)
+class RevisaodAdmin(admin.ModelAdmin):
+    list_display=['data_agendada','concluida']
+
+
+@admin.register(Flashcard)
+class FlashcardAdmin(admin.ModelAdmin):
+    list_display=['titulo', 'content',]
+    pass
+
