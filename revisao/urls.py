8109382538cf8dfb9ecaf66fe4af055fd8204d99
Clarisse <mclarissetc@gmail.com>
2023-10-17 16:22:47 -0300

    flashcards

--- revisao/urls.py
+++ revisao/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from .views import form_flashcard,  detalhes_flashcard,  revisoes_por_dia_da_semana,  tudo, calendario, lista_revisao, indexrevisao
+from .views import  detalhes_flashcard,  revisoes_por_dia_da_semana,  tudo, calendario, lista_revisao, indexrevisao, flashcards
 
 
 urlpatterns = [
-    path('form_flashcard/', form_flashcard, name="form_flashcard"),
     path('flashcard/<int:flashcard_id>/', detalhes_flashcard, name='detalhes_flashcard'),
     path('lista_revisao', lista_revisao, name='lista_revisao'),
     path('revisoes_por_dia_da_semana/', revisoes_por_dia_da_semana, name='revisoes_por_dia_da_semana'),
     path('calendario', calendario, name='calendario'),
     path('tudo/', tudo, name='tudo'),
     path('indexrevisao', indexrevisao, name='indexrevisao'),
+    path('flashcards/', flashcards, name='flashcards')
 ]
