40a49897df8c75adc48b4dd234c5cc025e38415c
Thamires-Teixeira <121453460+Thamires-Teixeira@users.noreply.github.com>
2023-11-28 14:23:16 -0300

    a

--- questoes/models.py
+++ questoes/models.py
@@ -4,12 +4,28 @@ class Disciplina(models.Model):
     nome = models.CharField(max_length=100, default="")
     def __str__(self):
         return self.nome
-    
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    questoes_certas = models.PositiveIntegerField(default=0)
+    questoes_erradas = models.PositiveIntegerField(default=0)
+
+    def taxa_acerto(self):
+        num_questoes = self.num_questoes()
+        if num_questoes > 0:
+            return round((self.questoes_certas / num_questoes) * 100, 2)
+        return 0
+
+    def num_questoes(self):
+        return self.questoes_certas + self.questoes_erradas
+
 class Assunto(models.Model):
     disciplina = models.ForeignKey(Disciplina, on_delete=models.CASCADE)
     assunto = models.CharField(max_length=150, default="")
     def __str__(self):
         return self.assunto
+
 
 class Questao(models.Model):
     disciplina = models.ForeignKey(Disciplina, on_delete=models.CASCADE)
@@ -20,8 +36,13 @@ class Questao(models.Model):
     ordem = models.PositiveIntegerField(default=0) 
     respondida = models.BooleanField(default=False)  
 
+    # Adicione os campos para armazenar o número de questões certas e erradas
+    questoes_certas = models.PositiveIntegerField(default=0)
+    questoes_erradas = models.PositiveIntegerField(default=0)
+
     def __str__(self):
         return f"Questão {self.id} - {self.disciplina.nome}"
+
     
 class Alternativa(models.Model):
     questao = models.ForeignKey('Questao', related_name='alternativas', on_delete=models.CASCADE)
--- questoes/views.py
+++ questoes/views.py
@@ -1,18 +1,59 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Disciplina, Assunto, Questao, Alternativa
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from .models import Disciplina, Assunto, Questao, Alternativa, UserProfile
+from django.contrib.auth.models import User
+import json
+
+
+
+def estatisticas(request):
+    # Obtenha os dados iniciais
+    questoes_certas = 0
+    questoes_erradas = 0
+    taxa_acerto = 0
+    num_questoes = 0
+
+    # Se o usuário estiver autenticado, obtenha os dados reais
+    if request.user.is_authenticated:
+        user_profile, created = UserProfile.objects.get_or_create(user=request.user)
+        questoes_certas = user_profile.questoes_certas
+        questoes_erradas = user_profile.questoes_erradas
+        num_questoes = questoes_certas + questoes_erradas
+        # Evite a divisão por zero
+        if num_questoes > 0:
+            taxa_acerto = round((questoes_certas / num_questoes) * 100, 2)
+
+    # Envie os dados para o template
+    data = {
+        'questoes_certas': questoes_certas,
+        'questoes_erradas': questoes_erradas,
+        'taxa_acerto': taxa_acerto,
+        'num_questoes': num_questoes,
+    }
+
+    return render(request, 'questoes/pages/estatisticas.html', data)
+
 
 @login_required
 def lista_questoes(request):
     disciplinas = Disciplina.objects.all()
     questoes = Questao.objects.all()
+
+    # Adicione a lógica de paginação aqui
+    questoes_por_pagina = 5
+    paginator = Paginator(questoes, questoes_por_pagina)
+    page_number = request.GET.get('page')
+    page = paginator.get_page(page_number)
+
     context = {
         'disciplinas': disciplinas, 
-        'questoes': questoes,
+        'questoes': page,
     }
     return render(request, 'questoes/pages/lista_questoes.html', context)
 
+# Defina a função filtro_questoes
 def filtro_questoes(request):
     disciplinas = Disciplina.objects.all()
 
@@ -44,8 +85,13 @@ def filtro_questoes(request):
         'assunto_selecionado': int(assunto_id) if assunto_id else None, 
     })
 
+# Restante do seu código...
+
+
+@login_required
 def verificar_resposta(request, questao_id):
     questao = get_object_or_404(Questao, pk=questao_id)
+    user_profile, created = UserProfile.objects.get_or_create(user=request.user)
 
     if request.method == 'POST':
         alternativa_id = request.POST.get('alternativa')
@@ -53,35 +99,51 @@ def verificar_resposta(request, questao_id):
 
         if alternativa_selecionada.correta:
             mensagem = 'acertou!'
+            user_profile.questoes_certas += 1
         else:
             mensagem = 'errou.'
+            user_profile.questoes_erradas += 1
 
         questao.respondida = True
         questao.save()
+        user_profile.save()
 
-        return render(request, 'questoes/resposta.html', {'mensagem': mensagem})
+        # Retorne os dados atualizados para o gráfico
+        data = {
+            'questoes_certas': user_profile.questoes_certas,
+            'questoes_erradas': user_profile.questoes_erradas,
+            'taxa_acerto': user_profile.taxa_acerto(),  # Adicione esse método ao seu modelo UserProfile
+            'num_questoes': user_profile.num_questoes(),
+        }
+
+        # Se preferir, pode retornar a resposta como JSON
+        return JsonResponse(data)
 
     return redirect('questoes', questao_id=questao_id)
 
-def estatisticas(request):
-    return render(request, 'questoes/pages/estatisticas.html')
+
+
+
+
 
 def indexquestoes(request):
     return render(request, 'questoes/pages/indexquestoes.html')
 
-@login_required
 def grafico(request, usuario_id):
     user = User.objects.get(id=usuario_id)
-    questoes_certas = user.questoes.filter(correta=True).count()
-    questoes_erradas = user.questoes.filter(correta=False).count()
-   
-    data = {
-        'questoes_certas': questoes_certas,
-        'questoes_erradas': questoes_erradas,
-    }
-   
-    data_json = json.dumps(data)
-   
-    return render(request, 'questoes/partials/grafico.html', {'data_json': data_json})
+    disciplinas = Disciplina.objects.all()
+
+    dados_disciplinas = []
+    for disciplina in disciplinas:
+        questoes_certas = user.questoes.filter(disciplina=disciplina, correta=True).count()
+        questoes_erradas = user.questoes.filter(disciplina=disciplina, correta=False).count()
+
+        dados_disciplinas.append({
+            'disciplina_nome': disciplina.nome,
+            'questoes_certas': questoes_certas,
+            'questoes_erradas': questoes_erradas,
+        })
+
+    return render(request, 'questoes/partials/valores_disciplinas.html', {'dados_disciplinas': dados_disciplinas})
 
 
